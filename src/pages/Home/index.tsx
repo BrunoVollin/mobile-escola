e8d9ca37c47ab4ec318037125c23d34de0f3d78b
Bruno Almeida <61352086+BrunoVollin@users.noreply.github.com>
2023-03-31 18:00:33 -0300

    feat(HomeTeacher): integrate with backend

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,27 +1,50 @@
 import { Text } from "react-native";
 import { Wrapper, Container } from "./styles";
 import { useNavigation } from "@react-navigation/native";
-import { useContext } from "react";
+import { useCallback, useContext } from "react";
 import { AuthContext } from "./../../contexts/AuthContext";
 import { Button } from "react-native-paper";
+import TeacherService from "../../services/teacher";
+import { useFocusEffect } from "expo-router";
+import Card from "../../components/Card";
+import { useState } from "react";
+import { FlatList } from "react-native-gesture-handler";
 
 interface HomeProps {}
 
 export default function Home() {
   const navigation = useNavigation();
   const { user, signOut } = useContext(AuthContext);
+  const [classes, setClasses] = useState<[]>([]);
+
+  const handleGetClasses = async () => {
+    const response = await TeacherService.getClasses();
+    setClasses(response.classes);
+  };
+
+  useFocusEffect(
+    useCallback(() => {
+      handleGetClasses();
+    }, [])
+  );
 
   return (
     <>
       <Wrapper>
         <Container>
-          <Text>
-            Home: {"\n"}
-            {JSON.stringify(user)}
-          </Text>
-          <Button mode="contained" onPress={() => signOut()}>
-            Sair
-          </Button>
+          <FlatList
+            data={classes}
+            renderItem={({
+              item,
+            }: {
+              item: {
+                id: string;
+                name: string;
+                description: string;
+              };
+            }) => <Card title={item.name} subtitle={item.description} />}
+            keyExtractor={(item) => item.id}
+          />
         </Container>
       </Wrapper>
     </>
